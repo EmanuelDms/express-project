a213c0ee4a87f927376fd327987ac761f947c5f5
EmanuelDms <emanuel.duarte.maia@gmail.com>
2020-12-12 01:45:26 -0300

    feat: create CategoryController

---
+++ src/controllers/CategoryController.js
@@ -0,0 +1,71 @@
+import Category from "../models/Category";
+
+class CategoryController {
+  // Show all categories
+  async index(req, res) {
+    const categories = await Category.find({})
+    return res.json(categories);
+  }
+
+  // Create a new category
+  async store(req, res) {
+    const { name } = req.body;
+
+    const categoryExists = await Category.findOne({ name });
+
+    if (categoryExists) {
+      return res.status(400).json({ error: 'Category already exists.' })
+    }
+
+    const category = await Category.create({
+      name
+    });
+
+    return res.json(category)
+  }
+
+  // Update a category
+  async update(req, res) {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    const category = await Category.findById(id);
+
+    if (!category) {
+      return res.status(400).json({ error: 'Category does not found.' })
+    }
+
+    if (name && (name !== category.name)) {
+
+      const categoryExists = await Category.findOne({ name });
+
+      if (categoryExists) {
+        return res.status(400).json({ error: 'Category name already exists.' })
+      }
+    }
+
+    if (name === category.name) {
+      return res.status(400).json({ error: 'Category name is the same.' })
+    }
+
+    category.name = name;
+    await category.save();
+
+    return res.json(category)
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const category = await Category.findById(id);
+
+    if (!category) {
+      return res.status(400).json({ error: 'Category does not found.' })
+    }
+
+    await category.remove();
+    return res.status(204).send();
+  }
+}
+
+export default new CategoryController();
