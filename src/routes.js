53ec63f2519b00cadf1e687b19419bb5846f8ae5
EmanuelDms <emanuel.duarte.maia@gmail.com>
2020-12-12 18:52:15 -0300

    chore & feat: remove uuidv4 & add product routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,20 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+
 import CategoryController from './controllers/CategoryController';
+import ProductController from './controllers/ProductController';
 
 const routes = new Router();
 
+// Categories routes
 routes.get('/categories', CategoryController.index)
 routes.post('/categories', CategoryController.store)
 routes.put('/categories/:id', CategoryController.update)
 routes.delete('/categories/:id', CategoryController.delete)
+
+// Product routes
+routes.get('/products', ProductController.index)
+routes.post('/products', ProductController.store)
+routes.put('/products/:id', ProductController.update)
+routes.delete('/products/:id', ProductController.delete)
 
 export default routes;
