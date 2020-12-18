5c198522bc18bc7c5aa25cad5946b5bf8e5434ce
EmanuelDms <emanuel.duarte.maia@gmail.com>
2020-12-18 18:30:09 -0300

    refactor: changing paradigms

--- src/app.js
+++ src/app.js
@@ -2,12 +2,23 @@ import express from 'express';
 import routes from './routes';
 import "./database";
 
-const app = express();
+class App{
+  constructor() {
+    // setup
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+  
+  middlewares(){
+    // Faz com que as requisições entendam json (middlewares de configuração)
+    this.server.use(express.json())
+  }
+  
+  routes(){
+    // Usa as rotas
+    this.server.use(routes);
+  }
+}
 
-// Faz com que as requisições entendam json
-app.use(express.json());
-
-// Usa as rotas
-app.use(routes);
-
-export default app;
+export default new App().server;
